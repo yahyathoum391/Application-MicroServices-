f7d68d7ae1f555e791a10f28166697d3ffeaf70a
yahyathoum391 <74272615+yahyathoum391@users.noreply.github.com>
2021-02-21 01:06:55 +0100

    Add files via upload

---
+++ inventory-service/inventory-service/src/main/java/org/sid/inventoryservice/InventoryServiceApplication.java
@@ -0,0 +1,35 @@
+package org.sid.inventoryservice;
+import org.sid.inventoryservice.entities.Product;
+import org.sid.inventoryservice.repository.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryServiceApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner start(ProductRepository productRepository,
+                            RepositoryRestConfiguration restConfiguration) {
+
+       restConfiguration.exposeIdsFor(Product.class);
+        return args -> {
+
+            productRepository.save(new Product(null, "Ordinateur", 70000, 10));
+
+            productRepository.save(new Product(null, "Imprimante", 80000, 5));
+
+            productRepository.save(new Product(null, "SmartTv", 9000, 4));
+
+            productRepository.findAll().forEach(p -> {
+                System.out.println(p.getName() + "   " + p.getPrice());
+            });
+        };
+    }
+}
