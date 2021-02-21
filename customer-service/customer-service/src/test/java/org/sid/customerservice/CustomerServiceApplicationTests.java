7edcdbf609809c9b82cca31c5107c6b0ac86659d
yahyathoum391 <74272615+yahyathoum391@users.noreply.github.com>
2021-02-21 01:12:11 +0100

    Add files via upload

---
+++ customer-service/customer-service/src/test/java/org/sid/customerservice/CustomerServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package org.sid.customerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+    CustomerServiceApplicationTests() {
+
+    }
+
+    @Test
+    void contextLoads() {
+    }
+
+}
